153b3d9c733e8a28f05c3466c56068b011e112d3
yaneysi oleta mendez <yaneysioletamendez@gmail.com>
2024-06-24 08:02:21 -0600

    Se agrago el archivo index del proyecto y se actualizo el archivo readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # demoyane
 este repositorio nos servirá de practica para la tecnología git
+#Tecnologias 
+Las tecnoligias que se utilizan para este proyecto son php y mysql.
---
+++ index.php
@@ -0,0 +1,12 @@
+<DOCTYPE html>
+    <html lang="en">
+       <meta charset="UTF-8">
+       <meta name="viewport" contente="width=device-width, initial-scale=1.0">
+       <title> Pagina demo </title>
+</head>
+<body>
+    <h2>
+        Welcome to Yaneysi's page.
+</h2>
+</body>
+</html>
